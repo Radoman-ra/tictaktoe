241e64949da73680f4aebfb4e55cef71a27f75d7
Radoman-ra <rmaksim886@gmail.com>
2024-06-30 20:41:46 +0200

    add colors to X and O comments fix simlify the win notification => make is a separate functino

--- tictaktoe.py
+++ tictaktoe.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import Toplevel
 from tkinter import messagebox
+
 import pygame
 
 pygame.init()
@@ -20,18 +21,26 @@ def end_music():
     pygame.mixer.music.load('end.mp3')
     pygame.mixer.music.play()
 
+
 def error_music():
     pygame.mixer.music.load('eror.mp3')
     pygame.mixer.music.play()
+
 
 def main_music():
     pygame.mixer.music.load('main_music.mp3')
     pygame.mixer.music.play(-1)
 
+
 def restart():
     global window
     window.destroy()
     menu.deiconify()
+def win_notification():
+    end_music()
+    messagebox.showinfo("THE END", f"{turn} win!!!!!")
+    restart()
+
 
 def button_click(event):
     global turn
@@ -40,6 +49,12 @@ def button_click(event):
         counter -= 1
         if counter != 0:
             event.config(text=turn)
+
+            if turn == "X":
+                event.config(fg="red")
+            else:
+                event.config(fg="blue")
+
             info = event.grid_info()
             row_inf = info["row"]
             col_inf = info["column"]
@@ -61,8 +76,9 @@ def button_click(event):
             right_up_deagonal_check = 1
             left_down_right_up_deagonal_counter = 1
 
-            # Проверяем горизонталь
-            # Проверяем слева
+            # Checking the horizontal
+            # Check on the left
+
             while left_check < win:
                 if col_inf - left_check >= 0:
                     if buttons_list[row_inf][col_inf - left_check] == turn:
@@ -70,14 +86,14 @@ def button_click(event):
                         print(right_and_left_counter)
                         left_check += 1
                         if left_check == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                     else:
                         break
                 else:
                     break
-            # Проверяем справа + общ
+
+            # Check on the right + general
+
             while right_check < win:
                 if col_inf + right_check < grid_size:
                     if buttons_list[row_inf][col_inf + right_check] == turn:
@@ -85,20 +101,17 @@ def button_click(event):
                         print(right_and_left_counter)
                         right_check += 1
                         if right_check == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                         elif right_and_left_counter == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                     else:
                         break
                 else:
                     break
 
-            # Проверяем вертикаль
-            # Проверяем сверху
+            # Checking the vertical
+            # We check from above
+
             while up_check < win:
                 if row_inf - up_check >= 0:
                     if buttons_list[row_inf - up_check][col_inf] == turn:
@@ -106,14 +119,14 @@ def button_click(event):
                         print(up_down)
                         up_check += 1
                         if up_check == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                     else:
                         break
                 else:
                     break
-            # Проверяем снизу + общ
+
+            # Check from below + general
+
             while down_check < win:
                 if row_inf + down_check < grid_size:
                     if buttons_list[row_inf + down_check][col_inf] == turn:
@@ -121,20 +134,17 @@ def button_click(event):
                         print(up_down)
                         down_check += 1
                         if down_check == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                         elif up_down == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                     else:
                         break
                 else:
                     break
 
-            # Проверяем диагональ
-            # Слева сверху
+            # Checking the diagonal
+            # Top left
+
             while left_up_deagonal_check < win:
                 if row_inf + left_up_deagonal_check < grid_size and col_inf - left_up_deagonal_check >= 0:
                     if buttons_list[row_inf + left_up_deagonal_check][col_inf - left_up_deagonal_check] == turn:
@@ -142,14 +152,14 @@ def button_click(event):
                         print(left_up_right_down_deagonal_counter)
                         left_up_deagonal_check += 1
                         if left_up_deagonal_check == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                     else:
                         break
                 else:
                     break
-            # Справа снизу + общ
+
+            # Bottom right + general
+
             while right_down_deagonal_check < win:
                 if row_inf - right_down_deagonal_check >= 0 and col_inf + right_down_deagonal_check < grid_size:
                     if buttons_list[row_inf - right_down_deagonal_check][col_inf + right_down_deagonal_check] == turn:
@@ -157,20 +167,17 @@ def button_click(event):
                         print(left_up_right_down_deagonal_counter)
                         right_down_deagonal_check += 1
                         if right_down_deagonal_check == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                         elif left_up_right_down_deagonal_counter == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                     else:
                         break
                 else:
                     break
 
-            # Проверяем диагональ
-            # Слева снизу
+            # Checking the diagonal
+            # Bottom left
+
             while left_down_deagonal_check < win:
                 if row_inf - left_down_deagonal_check >= 0 and col_inf - left_down_deagonal_check >= 0:
                     if buttons_list[row_inf - left_down_deagonal_check][col_inf - left_down_deagonal_check] == turn:
@@ -178,14 +185,14 @@ def button_click(event):
                         print(left_down_right_up_deagonal_counter)
                         left_down_deagonal_check += 1
                         if left_down_deagonal_check == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                     else:
                         break
                 else:
                     break
-            # Справа сверху + общ
+
+            # Top right + general
+
             while right_up_deagonal_check < win:
                 if row_inf + right_up_deagonal_check < grid_size and col_inf + right_up_deagonal_check < grid_size:
                     if buttons_list[row_inf + right_up_deagonal_check][col_inf + right_up_deagonal_check] == turn:
@@ -193,13 +200,9 @@ def button_click(event):
                         print(left_down_right_up_deagonal_counter)
                         right_up_deagonal_check += 1
                         if right_up_deagonal_check == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                         elif left_down_right_up_deagonal_counter == win:
-                            end_music()
-                            messagebox.showinfo("THE END", f"{turn} win!!!!!")
-                            restart()
+                            win_notification()
                     else:
                         break
                 else:
@@ -211,8 +214,9 @@ def button_click(event):
                 turn = "X"
         else:
             error_music()
-            messagebox.showinfo("THE END", f"идите нахуй с такой игрой")
+            messagebox.showinfo("THE END", f"The game ended in a draw")
             restart()
+
 
 def apply_settings():
     try:
@@ -220,7 +224,7 @@ def apply_settings():
         grid_size = int(entry_Grid_size.get())
         win = int(entry_win.get())
         counter = grid_size * grid_size
-        print(f"Применены настройки: grid_size={grid_size}, win={win}")
+        print(f"Settings applied: grid_size={grid_size}, win={win}")
         if win <= grid_size and win > 1 and grid_size > 1:
             global window
             main_music()
@@ -243,14 +247,17 @@ def apply_settings():
                     )
                     button.grid(column=col, row=row, padx=1, pady=2)
                     button.bind("<Button-1>", lambda event, btn=button: button_click(btn))
-                    buttons_row.append(button)  # Добавляем кнопку в текущий ряд
-                buttons_list.append(buttons_row)  # Добавляем ряд в список кнопок
+                    buttons_row.append(button)
+                buttons_list.append(buttons_row)
 
-            menu.withdraw()  # Скрываем основное окно
+            menu.withdraw()
         else:
+            error_music()
             messagebox.showinfo("ERROR", "WIN is greater than GRID SIZE")
     except ValueError:
+        error_music()
         messagebox.showinfo("ERROR", "Please enter valid numbers for Grid Size and Win conditions")
+
 
 # -----------------
 # MENU
@@ -267,13 +274,12 @@ label_Grid_size.grid(row=0, column=0, padx=5, pady=5)
 entry_Grid_size = tk.Entry(frame_Grid_size, width=10)
 entry_Grid_size.grid(row=0, column=1, padx=5, pady=5)
 
-# Поле ввода для параметра win
 frame_win = tk.Frame(menu)
 frame_win.pack(pady=5)
 label_win = tk.Label(frame_win, text="Win:")
 label_win.grid(row=0, column=0, padx=5, pady=5)
 entry_win = tk.Entry(frame_win, width=10)
 entry_win.grid(row=0, column=1, padx=5, pady=5)
-button_apply = tk.Button(menu, text="Применить настройки", command=apply_settings)
+button_apply = tk.Button(menu, text="Apply Settings", command=apply_settings)
 button_apply.pack(pady=20)
 menu.mainloop()
